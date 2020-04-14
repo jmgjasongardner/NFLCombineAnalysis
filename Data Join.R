fd00bff6f9eff76b3606a32a73dc7ab7acd310ab
Elijah Lipkin <elipkin222@gmail.com>
2020-04-14 19:55:32 -0400

    Joining Data, All AV included

---
+++ Data Join.R
@@ -0,0 +1,115 @@
+library(plyr)
+library(dplyr)
+
+AVPlayers <- read.csv("AVPlayers.csv")
+DataUpdate1 <- read.csv("DataUpdate1.csv")
+
+DataUpdate1$Position <- as.character(DataUpdate1$Position)
+AVPlayers$position <- as.character(AVPlayers$position)
+
+AVPlayers$Name <- AVPlayers$full_name
+AVPlayers$Position <- AVPlayers$position
+
+AVPlayers <- AVPlayers[, -c(1, 7, 8, 12, 13)]
+
+for(val in 1:length(DataUpdate1$Position)) {
+  if(DataUpdate1$Position[val] == "OT") {
+    DataUpdate1$Position[val] <- "OL"
+  }
+  if(DataUpdate1$Position[val] == "OG") {
+    DataUpdate1$Position[val] <- "OL"
+  }
+  if(DataUpdate1$Position[val] == "C") {
+    DataUpdate1$Position[val] <- "OL"
+  }
+  if(DataUpdate1$Position[val] == "ILB") {
+    DataUpdate1$Position[val] <- "LB"
+  }
+  if(DataUpdate1$Position[val] == "OLB") {
+    DataUpdate1$Position[val] <- "LB"
+  }
+  if(DataUpdate1$Position[val] == "FS") {
+    DataUpdate1$Position[val] <- "DB"
+  }
+  if(DataUpdate1$Position[val] == "SS") {
+    DataUpdate1$Position[val] <- "DB"
+  }
+  if(DataUpdate1$Position[val] == "CB") {
+    DataUpdate1$Position[val] <- "DB"
+  }
+  if(DataUpdate1$Position[val] == "DE") {
+    DataUpdate1$Position[val] <- "DL"
+  }
+  if(DataUpdate1$Position[val] == "DT") {
+    DataUpdate1$Position[val] <- "DL"
+  }
+}
+
+for(val in 1:length(AVPlayers$Position)) {
+  if(AVPlayers$Position[val] == "OLB") {
+    AVPlayers$Position[val] <- "LB"
+  }
+  if(AVPlayers$Position[val] == "ILB") {
+    AVPlayers$Position[val] <- "LB"
+  }
+  if(AVPlayers$Position[val] == "NT") {
+    AVPlayers$Position[val] <- "DL"
+  }
+  if(AVPlayers$Position[val] == "T") {
+    AVPlayers$Position[val] <- "OL"
+  }
+  if(AVPlayers$Position[val] == "G") {
+    AVPlayers$Position[val] <- "OL"
+  }
+  if(AVPlayers$Position[val] == "OLB") {
+    AVPlayers$Position[val] <- "LB"
+  }
+  if(AVPlayers$Position[val] == "DE") {
+    AVPlayers$Position[val] <- "DL"
+  }
+  if(AVPlayers$Position[val] == "DT") {
+    AVPlayers$Position[val] <- "DL"
+  }
+  if(AVPlayers$Position[val] == "CB") {
+    AVPlayers$Position[val] <- "DB"
+  }
+  if(AVPlayers$Position[val] == "FS") {
+    AVPlayers$Position[val] <- "DB"
+  }
+  if(AVPlayers$Position[val] == "SS") {
+    AVPlayers$Position[val] <- "DB"
+  }
+  if(AVPlayers$Position[val] == "S") {
+    AVPlayers$Position[val] <- "DB"
+  }
+  if(AVPlayers$Position[val] == "C") {
+    AVPlayers$Position[val] <- "OL"
+  }
+  if(AVPlayers$Position[val] == "OT") {
+    AVPlayers$Position[val] <- "OL"
+  }
+  if(AVPlayers$Position[val] == "LS") {
+    AVPlayers$Position[val] <- "OL"
+  }
+  if(AVPlayers$Position[val] == "FB") {
+    AVPlayers$Position[val] <- "RB"
+  }
+}
+
+# unique(DataUpdate1$Position)
+# unique(AVPlayers$Position)
+
+Data <- join(AVPlayers, DataUpdate1, by = c("Name", "Position"))
+
+Data <- Data[-c(56, 151, 248, 354, 416, 419, 477, 661, 663, 722, 916, 985, 
+                989:990, 1060, 1019, 1357, 1381, 1433, 1441, 1469, 1697, 1753, 
+                1771, 1803, 1810:1811, 1845, 1873, 1930, 2043, 2050, 2107, 2190, 
+                2455, 2540, 2562, 2604, 2621, 2721, 2768, 2782, 2800, 2897, 2916, 
+                3028, 3809, 3523, 3653, 3736, 3799), -c(6:8, 14)]
+
+colnames(Data) <- c("Draft Year", "Team", "Pick Round", "Pick Number", "PlayerID",
+                    "Games Played", "Total AV", "AV Per 16", "Name", "Position",
+                    "College", "Height", "Weight", "Wonderlic", "40YD", "Bench", 
+                    "Vert", "Broad", "Shuttle", "3Cone", "Pro Bowls", "All Pro")
+
+write.csv(Data, "DataUpdate3.csv")

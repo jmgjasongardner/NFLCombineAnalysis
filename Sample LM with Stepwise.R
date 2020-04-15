a8e825c669c565da2df6e48b94866a1a65152649
jmgjasongardner <jmgjasongardner@gmail.com>
2020-04-15 15:51:32 -0600

    Updated Bayesian with fixed wording on variables

--- Sample LM with Stepwise.R
+++ Sample LM with Stepwise.R
@@ -2,15 +2,15 @@ library(MASS)
 
 rb <- read.csv("RBs.csv")
 
-rb.full <- rb[is.na(rb$X40.YD) == FALSE, ]
+rb.full <- rb[is.na(rb$Forty) == FALSE, ]
 rb.full <- rb.full[is.na(rb.full$Weight) == FALSE, ]
 rb.full <- rb.full[is.na(rb.full$Height) == FALSE, ]
-rb.full <- rb.full[is.na(rb.full$X3Cone) == FALSE, ]
+rb.full <- rb.full[is.na(rb.full$ThreeCone) == FALSE, ]
 rb.full <- rb.full[is.na(rb.full$Broad) == FALSE, ]
 rb.full <- rb.full[is.na(rb.full$Bench) == FALSE, ]
 rb.full <- rb.full[is.na(rb.full$Shuttle) == FALSE, ]
 
-rblm2 <- lm(AV16 ~ Height + Weight + X40.YD + X3Cone + Broad + Bench + Shuttle, 
+rblm2 <- lm(AV16 ~ Height + Weight + Forty + ThreeCone + Broad + Bench + Shuttle, 
             data = rb.full)
 summary(rblm2)
 summary(stepAIC(object = rblm2, direction = c("backward")))
